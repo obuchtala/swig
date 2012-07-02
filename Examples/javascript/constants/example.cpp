03e75a098b5497ef4e3f34a8e7ecb4950424e59d
neha1 <neha1@626c5289-ae23-0410-ae9c-e8d60b6d4f22>
2012-07-02 21:32:57 +0000

    Cleaned up example 'constants'.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-javascript@13245 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/javascript/constants/example.cpp
+++ Examples/javascript/constants/example.cpp
@@ -1,10 +1,1 @@
-#include <iostream>
-using namespace std;
-
-int    ICONST = 42;
-float  FCONST = 2.1828;
-char   CCONST = 'x';
-char   CCONST2 = '\n';
-string SCONST = "Hello World";
-string SCONST2 = "\"Hello World\"";
 
--- Examples/javascript/constants/runme.js
+++ Examples/javascript/constants/runme.js
@@ -6,7 +6,3 @@ print("CCONST  = " + cvar.CCONST + " (should be 'x')\n");
 print("CCONST2 = " + cvar.CCONST2, " (this should be on a new line)\n"); 
 print("SCONST  = " + cvar.SCONST + " (should be 'Hello World')\n");
 print("SCONST2 = " + cvar.SCONST2 + " (should be '\"Hello World\"')\n");
-print("EXPR    = " + cvar.EXPR +    " (should be 48.5484)\n");
-print("iconst  = " + cvar.iconst + " (should be 37)\n"); 
-print("fconst  = " + cvar.fconst + " (should be 3.14)\n"); 
-
