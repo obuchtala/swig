5877bdb13dbbf5f18b9912a61214ba9e59e5f665
neha1 <neha1@626c5289-ae23-0410-ae9c-e8d60b6d4f22>
2012-07-02 21:24:32 +0000

    Minor clean up in example 'class'.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-javascript@13242 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/javascript/class/example.cpp
+++ Examples/javascript/class/example.cpp
@@ -7,11 +7,9 @@ Circle::Circle(): radius(1.0) {}
 
 Circle::Circle(double r): radius(r) {
     std::cout << "created Circle with r=" << radius << std::endl;
-    std::cout << "ctor for " << (long) this << std::endl;
 }
 
 double Circle::area() {
     std::cout << "Circle::area called, r=" << radius << std::endl;
-    std::cout << "calling on " << (long) this << std::endl;
     return M_PI*radius*radius;
 }
