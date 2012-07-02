346b7a64de41bf56c518253b51a66055d6ac5ba3
neha1 <neha1@626c5289-ae23-0410-ae9c-e8d60b6d4f22>
2012-07-02 21:09:40 +0000

    Added an example for constant.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-javascript@13236 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Examples/javascript/constants/example.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int    ICONST = 42;
+float  FCONST = 2.1828;
+char   CCONST = 'x';
+char   CCONST2 = '\n';
+string SCONST = "Hello World";
+string SCONST2 = "\"Hello World\"";
+
---
+++ Examples/javascript/constants/example.h
@@ -0,0 +1,6 @@
+#define    ICONST      42
+#define    FCONST      2.1828
+#define    CCONST      'x'
+#define    CCONST2     '\n'
+#define    SCONST      "Hello World"
+#define    SCONST2     "\"Hello World\""
