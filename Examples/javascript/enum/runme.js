a83f188e38fdaace7f2511f9cb26046fdb7fb7a4
neha1 <neha1@626c5289-ae23-0410-ae9c-e8d60b6d4f22>
2012-07-09 16:54:06 +0000

    Added example for enums.
    From: Neha Narang <narangneha03@gmail.com>
    
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-javascript@13276 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Examples/javascript/enum/runme.js
@@ -0,0 +1,5 @@
+// enum value BLUE of enum color is accessed as property of cvar
+print("cvar.BLUE= " + cvar.BLUE);
+
+// enum value LUDICROUS of enum Foo::speed is accessed as as property of cvar
+print("Foo.LUDICROUS= " + Foo.LUDICROUS); 
