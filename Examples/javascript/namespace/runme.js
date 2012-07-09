bc0255b0b35b81dad72bd670d1bb873acbfebf06
neha1 <neha1@626c5289-ae23-0410-ae9c-e8d60b6d4f22>
2012-07-09 16:55:17 +0000

    Added example for namespaces.
    From: Neha Narang <narangneha03@gmail.com>
    
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-javascript@13278 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Examples/javascript/namespace/runme.js
@@ -0,0 +1,11 @@
+ print("Global variable Foo=" + cvar.Foo);
+ cvar.Foo = 5;
+ print("Variable Foo changed to " + cvar.Foo);
+ print("GCD of number 6,18 is " + cvar.gcd(6,18));
+
+print("Creating some objects:");
+c = new Circle(10);
+print("area = " + c.area());
+
+
+
