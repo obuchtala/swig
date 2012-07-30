5e9a60b5690fef0d9d3d832bef65c40d2870e6a6
neha1 <neha1@626c5289-ae23-0410-ae9c-e8d60b6d4f22>
2012-07-30 18:14:35 +0000

    Adapted cpp_enum test with correct syntax.
    From: Neha Narang <narangneha03@gmail.com>
    
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-javascript@13455 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/javascript/cpp_enum_runme.js
+++ Examples/test-suite/javascript/cpp_enum_runme.js
@@ -2,26 +2,25 @@ var f = new cpp_enum.Foo()
 
 if(f.hola != cpp_enum.Hello){
   print(f.hola);
-  throw "Error"
+  throw "Error";
 }
 
 f.hola = cpp_enum.Foo.Hi
 if(f.hola != cpp_enum.Foo.Hi){
   print(f.hola);
-  throw "Error"
+  throw "Error";
 }
 
 f.hola = cpp_enum.Hello
 
 if(f.hola != cpp_enum.Hello){
   print(f.hola);
-  throw "Error"
+  throw "Error";
 }
 
 cpp_enum.Foo.hi = cpp_enum.Hello
 if(cpp_enum.Foo.hi != cpp_enum.Hello){
   print(cpp_enum.Foo.hi);
-  throw "Error"
+  throw "Error";
 }
 
-//throw "Not implemented yet";
