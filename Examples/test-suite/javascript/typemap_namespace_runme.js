4870e0d07cd31b37aee176b05a43e84693778d76
neha1 <neha1@626c5289-ae23-0410-ae9c-e8d60b6d4f22>
2012-07-30 18:27:23 +0000

    Fix bugs in test runner for test "typemap_namespace".
    From: Oliver Buchtala <oliver.buchtala@googlemail.com>
    
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-javascript@13472 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/javascript/typemap_namespace_runme.js
+++ Examples/test-suite/javascript/typemap_namespace_runme.js
@@ -1,7 +1,6 @@
 
-if (test1("hello") != "hello")
+if (typemap_namespace.test1("hello") != "hello")
     throw "RuntimeError";
 
-if (test2("hello") != "hello")
+if (typemap_namespace.test2("hello") != "hello")
     throw "RuntimeError";
-
